ec039f5fa1d843f375235da789a2fc84ec3896ff
Oliver Foggin <oliverfoggin@me.com>
2022-09-12 15:29:31 +0100

    Initial Commit

---
+++ Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Oliver Foggin on 12/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
