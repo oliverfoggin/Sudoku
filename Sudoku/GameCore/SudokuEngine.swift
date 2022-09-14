d6103bd4f93c726305d0c3957bc482be20ce744c
Oliver Foggin <oliverfoggin@me.com>
2022-09-14 17:36:20 +0100

    Update sudoku engine to be a bit easier to read

--- Sudoku/GameCore/SudokuEngine.swift
+++ Sudoku/GameCore/SudokuEngine.swift
@@ -1,5 +1,6 @@
 struct SudokuEngine {
 	private static let groups: [Set<Int>] = [
+		// boxes
 		[0,  1,  2,  9,  10, 11, 18, 19, 20],
 		[3,  4,  5,  12, 13, 14, 21, 22, 23],
 		[6,  7,  8,  15, 16, 17, 24, 25, 26],
@@ -9,26 +10,36 @@ struct SudokuEngine {
 		[54, 55, 56, 63, 64, 65, 72, 73, 74],
 		[57, 58, 59, 66, 67, 68, 75, 76, 77],
 		[60, 61, 62, 69, 70, 71, 78, 79, 80],
+
+		// rows
+		[0,  1,  2,  3,  4,  5,  6,  7,  8],
+		[9,  10, 11, 12, 13, 14, 15, 16, 17],
+		[18, 19, 20, 21, 22, 23, 24, 25, 26],
+		[27, 28, 29, 30, 31, 32, 33, 34, 35],
+		[36, 37, 38, 39, 40, 41, 42, 43, 44],
+		[45, 46, 47, 48, 49, 50, 51, 52, 53],
+		[54, 55, 56, 57, 58, 59, 60, 61, 62],
+		[63, 64, 65, 66, 67, 68, 69, 70, 71],
+		[72, 73, 74, 75, 76, 77, 78, 79, 80],
+
+		// columns
+		[0,  9,  18, 27, 36, 45, 54, 63, 72],
+		[1,  10, 19, 28, 37, 46, 55, 64, 73],
+		[2,  11, 20, 29, 38, 47, 56, 65, 74],
+		[3,  12, 21, 30, 39, 48, 57, 66, 75],
+		[4,  13, 22, 31, 40, 49, 58, 67, 76],
+		[5,  14, 23, 32, 41, 50, 59, 68, 77],
+		[6,  15, 24, 33, 42, 51, 60, 69, 78],
+		[7,  16, 25, 34, 43, 52, 61, 70, 79],
+		[8,  17, 26, 35, 44, 53, 62, 71, 80],
 	]
 
 	static func visibleCells(from cell: Int) -> Set<Int> {
-		var cells: Set<Int> = []
-
-		let mod = cell % 9
-		stride(from: 0, to: 9*9, by: 9)
-			.map { $0 + mod }
-			.forEach { cells.insert($0) }
-
-		let div = cell / 9
-		let start = 9 * div
-		let end = start + 8
-		(start...end).forEach { cells.insert($0) }
-
 		groups.filter { $0.contains(cell) }
 			.flatMap { $0 }
-			.forEach { cells.insert($0) }
-
-		cells.remove(cell)
-		return cells
+			.filter { $0 != cell }
+			.reduce(into: []) { partialResult, i in
+				partialResult.insert(i)
+			}
 	}
 }
